ebe4c1895b2536235d0d7631d3ae1d36242700f5
DESKTOP-DQFREQE\Dorra <delmekki@lbconsulting.com.tn>
2018-10-22 12:37:01 +0300

    first commit

---
+++ farahhmetekv4/app/src/main/java/com/hmetek/farah/farahhmetekv4/EmptyActivity.java
@@ -0,0 +1,16 @@
+package com.hmetek.farah.farahhmetekv4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+public class EmptyActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_empty);
+
+    }
+}
